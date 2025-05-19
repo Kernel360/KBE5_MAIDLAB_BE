6db53c169316df5852ccc4e4d0f8ccf3611327b5
JINS-DE <rlawlstjd044@naver.com>
2025-05-19 13:42:20 +0900

    초기 설정

---
+++ MAIDLAB_BE/src/main/java/kernel/maidlab/MaidlabBeApplication.java
@@ -0,0 +1,13 @@
+package kernel.maidlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidlabBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaidlabBeApplication.class, args);
+	}
+
+}
---
+++ MAIDLAB_BE/src/test/java/kernel/maidlab/MaidlabBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kernel.maidlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaidlabBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
