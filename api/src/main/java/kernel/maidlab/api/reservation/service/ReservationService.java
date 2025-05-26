d94ccf24fc67aeab502285631394a2a96fdadbb8
JINS-DE <rlawlstjd044@naver.com>
2025-05-26 23:33:45 +0900

    refactor) 필요 없는 코드 리펙토링

--- api/src/main/java/kernel/maidlab/api/reservation/service/ReservationService.java
+++ api/src/main/java/kernel/maidlab/api/reservation/service/ReservationService.java
@@ -1,11 +1,8 @@
 package kernel.maidlab.api.reservation.service;
 
-
 import kernel.maidlab.api.reservation.dto.request.ReservationRequestDto;
 
-
 public interface ReservationService {
-	// List<AvailableManagerDto> getAvailableManagersList(LocalDateTime startTime);
 	void createReservation(ReservationRequestDto dto);
 
 	void checkTotalPrice(ReservationRequestDto dto);
