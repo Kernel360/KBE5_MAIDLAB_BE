12c83697728291d202ae741c2890d79e8be1770d
soooo-ning <ssoning13@gmail.com>
2025-05-27 13:37:34 +0900

    refactor: base entity 어노테이션으로 변경

--- common/src/main/java/kernel/maidlab/common/entity/Base.java
+++ common/src/main/java/kernel/maidlab/common/entity/Base.java
@@ -4,20 +4,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class Base {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
-	protected Base() {
-
-	}
-
-	public long getId() {
-		return id;
-	}
 
 }
