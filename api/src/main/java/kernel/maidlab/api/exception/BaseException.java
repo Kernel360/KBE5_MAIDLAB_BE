9c00cb805df221e753aee0db0e0818f2be6f026c
soooo-ning <ssoning13@gmail.com>
2025-05-28 14:00:04 +0900

    refactor: 응답 및 예외처리 수정

--- api/src/main/java/kernel/maidlab/api/exception/BaseException.java
+++ api/src/main/java/kernel/maidlab/api/exception/BaseException.java
@@ -4,7 +4,7 @@ import kernel.maidlab.common.enums.ResponseType;
 import lombok.Getter;
 
 @Getter
-public abstract class BaseException extends RuntimeException {
+public class BaseException extends RuntimeException {
 	private final ResponseType responseType;
 
 	public BaseException(ResponseType responseType) {
--- common/src/main/java/kernel/maidlab/common/dto/ResponseDto.java
+++ common/src/main/java/kernel/maidlab/common/dto/ResponseDto.java
@@ -1,13 +1,10 @@
 package kernel.maidlab.common.dto;
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import jakarta.servlet.http.HttpServletResponse;
 import kernel.maidlab.common.enums.ResponseType;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -31,40 +28,16 @@ public class ResponseDto<T> {
 		return responseType.getMessage();
 	}
 
-	public static <T> ResponseEntity<ResponseDto<T>> success(ResponseType responseType, T data) {
+	public static <T> ResponseEntity<ResponseDto<T>> success(T data) {
 		return ResponseEntity
 			.status(ResponseType.SUCCESS.getHttpStatus())
+			.body(new ResponseDto<>(ResponseType.SUCCESS, data));
+	}
+
+	public static <T> ResponseEntity<ResponseDto<T>> success(ResponseType responseType, T data) {
+		return ResponseEntity
+			.status(responseType.getHttpStatus())
 			.body(new ResponseDto<>(responseType, data));
 	}
 
-	public static <T> ResponseEntity<ResponseDto<T>> dataBaseError(ResponseType responseType) {
-		return ResponseEntity
-			.status(ResponseType.DATABASE_ERROR.getHttpStatus())
-			.body(new ResponseDto<>(responseType, null));
-	}
-
-	public static void sendErrorResponse(HttpServletResponse response, ResponseType responseType,
-		ObjectMapper objectMapper) throws IOException {
-		ResponseDto<Object> errorResponse = new ResponseDto<>(responseType, null);
-
-		response.setStatus(responseType.getHttpStatus().value());
-		response.setContentType("application/json;charset=UTF-8");
-
-		response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
-	}
-
-	public static void authorizationFailed(HttpServletResponse response, ObjectMapper objectMapper) throws
-		IOException {
-		sendErrorResponse(response, ResponseType.AUTHORIZATION_FAILED, objectMapper);
-	}
-
-	public static void doNotHavePermission(HttpServletResponse response, ObjectMapper objectMapper) throws
-		IOException {
-		sendErrorResponse(response, ResponseType.DO_NOT_HAVE_PERMISSION, objectMapper);
-	}
-
-	public static void validationFailed(HttpServletResponse response, ObjectMapper objectMapper) throws
-		IOException {
-		sendErrorResponse(response, ResponseType.VALIDATION_FAILED, objectMapper);
-	}
 }
