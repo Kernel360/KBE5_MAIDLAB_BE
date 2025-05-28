7408c94e8839487003e43ba10a53ee6516efb15c
JINS-DE <rlawlstjd044@naver.com>
2025-05-28 10:42:22 +0900

    refactor) 글로벌 익셉션 핸들러 처리로 dataBaseError 처리 로직 삭제.

--- common/src/main/java/kernel/maidlab/common/dto/ResponseDto.java
+++ common/src/main/java/kernel/maidlab/common/dto/ResponseDto.java
@@ -6,35 +6,28 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import kernel.maidlab.common.enums.ResponseType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
 import org.springframework.http.ResponseEntity;
 
 @Getter
 @AllArgsConstructor
 public class ResponseDto<T> {
 
-    @JsonIgnore
-    private final ResponseType responseType;
-    private final T data;
+	@JsonIgnore
+	private final ResponseType responseType;
+	private final T data;
 
-    @JsonProperty("code")
-    public String getCode() {
-        return responseType.getCode();
-    }
+	@JsonProperty("code")
+	public String getCode() {
+		return responseType.getCode();
+	}
 
-    @JsonProperty("message")
-    public String getMessage() {
-        return responseType.getMessage();
-    }
+	@JsonProperty("message")
+	public String getMessage() {
+		return responseType.getMessage();
+	}
 
-    public static <T> ResponseEntity<ResponseDto<T>> success(ResponseType responseType, T data) {
-        return ResponseEntity
-                .status(ResponseType.SUCCESS.getHttpStatus())
-                .body(new ResponseDto<>(responseType, data));
-    }
-
-    public static <T> ResponseEntity<ResponseDto<T>> dataBaseError(ResponseType responseType) {
-        return ResponseEntity
-                .status(ResponseType.DATABASE_ERROR.getHttpStatus())
-                .body(new ResponseDto<>(responseType, null));
-    }
+	public static <T> ResponseEntity<ResponseDto<T>> success(ResponseType responseType, T data) {
+		return ResponseEntity.status(ResponseType.SUCCESS.getHttpStatus()).body(new ResponseDto<>(responseType, data));
+	}
 }
