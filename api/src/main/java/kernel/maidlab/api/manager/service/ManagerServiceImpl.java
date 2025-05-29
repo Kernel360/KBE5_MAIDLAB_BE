c2f5a67f79d819895d2bc73ee68587f31c6ba471
NeedIDcard <jo3902@gmail.com>
2025-05-29 18:31:10 +0900

    feature/be/#18 managerservice uuid함수 uuid와 id를 변환할 수 있는 함수를 구현해두었으나 아직 사용할 곳이 없는것 같아 주석처리 해뒀습니다.

---
+++ api/src/main/java/kernel/maidlab/api/manager/service/ManagerServiceImpl.java
@@ -0,0 +1,33 @@
+package kernel.maidlab.api.manager.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import kernel.maidlab.api.auth.entity.Manager;
+import kernel.maidlab.api.auth.repository.ManagerRepository;
+
+@Service
+public class ManagerServiceImpl implements ManagerService {
+
+	private final ManagerRepository managerRepository;
+
+	public ManagerServiceImpl(ManagerRepository managerRepository) {
+		this.managerRepository = managerRepository;
+	}
+
+	//아직 쓰이는 곳이 없어 주석처리 합니다.
+	// @Override
+	// public Long GetIdByUuid(String uuid){
+	// 	Manager uuidmanager = managerRepository.findByUuid(uuid)
+	// 		.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 uuid입니다."));
+	// 	return uuidmanager.getId();
+	// }
+	//
+	// public String GetUuidByid(Long id){
+	// 	Optional<Manager> manager = managerRepository.findById(id);
+	// 	return manager.get().getUuid();
+	// }
+
+
+}
