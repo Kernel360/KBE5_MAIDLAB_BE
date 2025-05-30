a75cd943803e9689ed449ba353b6be434c483ca8
jaeho <jaho321@naver.com>
2025-05-30 10:50:27 +0900

    feat: 수요자의 매니저 수요(찜하기, 블랙리스트) 엔티티,관련DTO 추가

---
+++ api/src/main/java/kernel/maidlab/api/consumer/repository/ManagerPreferenceRepository.java
@@ -0,0 +1,27 @@
+package kernel.maidlab.api.consumer.repository;
+
+import kernel.maidlab.api.auth.entity.Manager;
+import kernel.maidlab.api.consumer.entity.ManagerPreference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ManagerPreferenceRepository extends JpaRepository<ManagerPreference, Long> {
+
+    @Query("SELECT DISTINCT m FROM ManagerPreference mp " +
+            "JOIN mp.manager m " +  // fetch 제거
+            "LEFT JOIN FETCH m.regions " +
+            "WHERE mp.consumer.id = :consumerId AND mp.preference = true")
+    List<Manager> findLikedManagersWithRegions(@Param("consumerId") Long consumerId);
+
+    @Query("SELECT DISTINCT m FROM ManagerPreference mp " +
+            "JOIN mp.manager m " +
+            "WHERE mp.consumer.id = :consumerId AND mp.preference = false ")
+    List<Manager> findBlackListedManagers(@Param("consumerId") Long consumerId);
+
+    long deleteByConsumerIdAndManagerId(Long consumerId, Long managerId);
+
+
+}
