b025aee34413066dc7561c46b4ce9cce0fa74a63
jaeho <jaho321@naver.com>
2025-06-02 16:34:42 +0900

    refactor: ConsumerBoardController api 경로명 변경

--- api/src/main/java/kernel/maidlab/api/board/controller/ConsumerBoardController.java
+++ api/src/main/java/kernel/maidlab/api/board/controller/ConsumerBoardController.java
@@ -7,15 +7,12 @@ import kernel.maidlab.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/consumers/board")
+@RequestMapping("/api/board")
 public class ConsumerBoardController {
 
     private final ConsumerBoardServiceImpl consumerBoardService;
