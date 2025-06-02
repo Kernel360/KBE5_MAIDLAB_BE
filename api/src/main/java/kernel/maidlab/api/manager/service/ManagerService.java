99f9ba9d0bd91b98f12681f94da6a402dbe815ae
NeedIDcard <jo3902@gmail.com>
2025-06-02 15:32:02 +0900

    feature/be/#65 admin manager 전체조회
    admin에서 manager를 리스트의 형태로 조회하는 기능 추가

--- api/src/main/java/kernel/maidlab/api/manager/service/ManagerService.java
+++ api/src/main/java/kernel/maidlab/api/manager/service/ManagerService.java
@@ -1,5 +1,10 @@
 package kernel.maidlab.api.manager.service;
 
+import org.springframework.data.domain.Page;
+
+import kernel.maidlab.api.manager.dto.ManagerListResponseDto;
+
 public interface ManagerService {
+	Page<ManagerListResponseDto> getManagerBypage(int page, int size);
 	// Long GetIdByUuid(String uuid);
 }
