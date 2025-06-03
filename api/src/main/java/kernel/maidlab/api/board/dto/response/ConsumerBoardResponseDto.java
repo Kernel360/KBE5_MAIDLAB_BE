703c05be5c282c8b1ffec68757ef6e327b79d63a
jaeho <jaho321@naver.com>
2025-06-04 01:12:15 +0900

    feat: 쿼리 조회를 받기 위한 dto, reponse응답 dto 추가

---
+++ api/src/main/java/kernel/maidlab/api/board/dto/response/ConsumerBoardResponseDto.java
@@ -0,0 +1,29 @@
+package kernel.maidlab.api.board.dto.response;
+
+import kernel.maidlab.api.board.dto.BoardQueryDto;
+import kernel.maidlab.api.board.eum.BoardType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConsumerBoardResponseDto {
+
+    private String title;
+    private String content;
+    private boolean answered;
+    private BoardType boardType;
+    //createAt - Base엔티티 수정시 추가 예정
+
+    // 정적 팩토리 메서드
+    public static ConsumerBoardResponseDto from(BoardQueryDto boardQueryDto){
+        ConsumerBoardResponseDto boardDto = new ConsumerBoardResponseDto();
+        boardDto.title = boardQueryDto.getTitle();
+        boardDto.content = boardQueryDto.getContent();
+        boardDto.answered = boardQueryDto.isAnswered();
+        boardDto.boardType = boardQueryDto.getBoardType();
+        return boardDto;
+    }
+}
