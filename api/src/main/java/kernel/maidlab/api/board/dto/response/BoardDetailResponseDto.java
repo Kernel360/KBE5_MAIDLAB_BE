791af2c0548e7645841a67ff6790e6c49f2372fe
jaeho <jaho321@naver.com>
2025-06-07 00:34:12 +0900

    refactor: BoardDetailResponseDto 컬럼명 변경

--- api/src/main/java/kernel/maidlab/api/board/dto/response/BoardDetailResponseDto.java
+++ api/src/main/java/kernel/maidlab/api/board/dto/response/BoardDetailResponseDto.java
@@ -13,28 +13,28 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ConsumerBoardDetailResponseDto {
+public class BoardDetailResponseDto {
 
     private String title;
     private String content;
-    private boolean answered;
+    private boolean isAnswered;
     private BoardType boardType;
 //    private LocalDateTime createAt
     private List<ImageDto> images;
     private AnswerResponseDto answer;
 
 
-    public static ConsumerBoardDetailResponseDto from (Board board, List<Image> images){
+    public static BoardDetailResponseDto from (Board board, List<Image> images){
 
-        return new ConsumerBoardDetailResponseDto(
+        return new BoardDetailResponseDto(
                 board.getTitle(),
                 board.getContent(),
-                board.isAnswered(),
+                board.getIsAnswered(),
                 board.getBoardType(),
                 images.stream()
                         .map(ImageDto::from)
                         .toList(),
-                board.isAnswered() ? AnswerResponseDto.from(board.getAnswer()) : null
+                board.getIsAnswered() ? AnswerResponseDto.from(board.getAnswer()) : null
         );
     }
 
