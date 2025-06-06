ec3d230764cc9a5297639c5929ae43dd5bb20e01
jaeho <jaho321@naver.com>
2025-06-07 00:28:19 +0900

    feat: ImageRepository 사진 삭제 기능 추가

--- api/src/main/java/kernel/maidlab/api/board/repository/ImageRepository.java
+++ api/src/main/java/kernel/maidlab/api/board/repository/ImageRepository.java
@@ -1,11 +1,19 @@
 package kernel.maidlab.api.board.repository;
 
+import feign.Param;
+import kernel.maidlab.api.board.entity.Board;
 import kernel.maidlab.api.board.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     List<Image> findAllByBoardId(Long boardId);
+
+    @Modifying
+    void deleteAllByBoard(Board board);
+
 }
