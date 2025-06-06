e4ae818fe0ee8b0aa2ea14c1f9130aa163de3468
jaeho <jaho321@naver.com>
2025-06-07 00:36:44 +0900

    refactor: BoardResponseDto 컬럼명 변경

--- api/src/main/java/kernel/maidlab/api/board/dto/response/BoardResponseDto.java
+++ api/src/main/java/kernel/maidlab/api/board/dto/response/BoardResponseDto.java
@@ -9,21 +9,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ConsumerBoardResponseDto {
+public class BoardResponseDto {
 
     private String title;
     private String content;
-    private boolean answered;
+    private boolean isAnswered;
     private BoardType boardType;
     //createAt - Base엔티티 수정시 추가 예정
 
     // 정적 팩토리 메서드
-    public static ConsumerBoardResponseDto from(BoardQueryDto boardQueryDto){
+    public static BoardResponseDto from(BoardQueryDto boardQueryDto){
 
-        ConsumerBoardResponseDto boardDto = new ConsumerBoardResponseDto();
+        BoardResponseDto boardDto = new BoardResponseDto();
         boardDto.title = boardQueryDto.getTitle();
         boardDto.content = boardQueryDto.getContent();
-        boardDto.answered = boardQueryDto.isAnswered();
+        boardDto.isAnswered = boardQueryDto.isAnswered();
         boardDto.boardType = boardQueryDto.getBoardType();
         return boardDto;
     }
