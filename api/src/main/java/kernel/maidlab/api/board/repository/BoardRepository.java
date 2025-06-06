724f2c4880dcf4e2001f0ca12a48447c6ae36a51
jaeho <jaho321@naver.com>
2025-06-06 23:28:58 +0900

    feat: BoardRepository idDeleted = false인 board 조회, 답변이 있을 경우 답변까지 함께 상세조회 기능 추가

--- api/src/main/java/kernel/maidlab/api/board/repository/BoardRepository.java
+++ api/src/main/java/kernel/maidlab/api/board/repository/BoardRepository.java
@@ -5,12 +5,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
 
-    // 나중에 쿼리dsl로 바꿀 예정
-    @Query("SELECT b FROM Board b LEFT JOIN FETCH b.answer a WHERE b.id = :boardId AND b.answered = true")
+    // todo: 나중에 쿼리dsl로 바꿀 예정
+    @Query("SELECT b, a " +
+            "FROM Board b " +
+            "LEFT JOIN FETCH b.answer a " +
+            "WHERE b.id = :boardId " +
+            "AND b.isAnswered = true " +
+            "AND b .isDeleted = false")
     Optional<Board> findBoardWithAnswerIfAnswered(@Param("boardId") Long boardId);
+
+    /**
+     * isDeleted = false만 조회하도록
+     */
+    // 전체 게시판 조회
+    List<Board> findAllByIsDeletedFalse();
+
+    // 단건 조회
+    Optional<Board> findByIdAndIsDeletedFalse(Long id);
 
 }
