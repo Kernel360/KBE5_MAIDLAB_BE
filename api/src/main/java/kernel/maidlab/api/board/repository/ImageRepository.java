e29082e390581e2d893e36d057ec13cbeee15cdc
jaeho <jaho321@naver.com>
2025-06-07 15:21:14 +0900

    feat: ImageRepository 게시판id와 같은 이미지 삭제 기능 추가

--- api/src/main/java/kernel/maidlab/api/board/repository/ImageRepository.java
+++ api/src/main/java/kernel/maidlab/api/board/repository/ImageRepository.java
@@ -1,11 +1,16 @@
 package kernel.maidlab.api.board.repository;
 
+import kernel.maidlab.api.board.entity.Board;
 import kernel.maidlab.api.board.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     List<Image> findAllByBoardId(Long boardId);
+
+    @Modifying
+    void deleteAllByBoard(Board board);
 }
