904242da66ad65ef73f2c6c7bce719ed60a32740
jaeho <jaho321@naver.com>
2025-06-07 17:36:04 +0900

    feat: FilterConfig board경로 추가

--- api/src/main/java/kernel/maidlab/api/config/FilterConfig.java
+++ api/src/main/java/kernel/maidlab/api/config/FilterConfig.java
@@ -17,7 +17,7 @@ public class FilterConfig {
 	public FilterRegistrationBean<Filter> jwtFilterRegistration(JwtFilter jwtFilter) {
 		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
 		registration.setFilter(jwtFilter);
-		registration.addUrlPatterns("/api/auth/*", "/api/consumer/*", "/api/manager/*"); // 인증이 필요한 경로만 필터링
+		registration.addUrlPatterns("/api/auth/*", "/api/consumer/*", "/api/manager/*", "/api/board/*"); // 인증이 필요한 경로만 필터링
 		registration.setName("jwtFilter");
 		registration.setOrder(2);
 
