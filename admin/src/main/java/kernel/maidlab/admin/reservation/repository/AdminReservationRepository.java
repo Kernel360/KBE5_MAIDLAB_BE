aa84dff95549f1f0c407cc9d81d8adca7d7d82df
NeedIDcard <jo3902@gmail.com>
2025-06-23 17:30:40 +0900

    feature/be/#183 오늘의 예약 수 조회 api 추가

--- admin/src/main/java/kernel/maidlab/admin/reservation/repository/AdminReservationRepository.java
+++ admin/src/main/java/kernel/maidlab/admin/reservation/repository/AdminReservationRepository.java
@@ -1,13 +1,14 @@
 package kernel.maidlab.admin.reservation.repository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import kernel.maidlab.common.entity.reservation.Reservation;
 
-public interface AdminReservationRepository extends JpaRepository<Reservation, Long> {
+public interface AdminReservationRepository extends JpaRepository<Reservation, Long>, AdminReservationRepositoryCustom {
 	Page<Reservation> findAllByReservationDateBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
 }
