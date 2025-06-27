e3a407e05c0e5363ebb0f1850be36cd5b1a47246
jaeho <jaho321@naver.com>
2025-06-27 13:39:20 +0900

    refactor: 프론트 MyPage에서 소설타입을 받기 위해 ConsumerMyPageDto 변경

--- common/src/main/java/kernel/maidlab/common/dto/consumer/ConsumerMyPageDto.java
+++ common/src/main/java/kernel/maidlab/common/dto/consumer/ConsumerMyPageDto.java
@@ -1,31 +1,26 @@
 package kernel.maidlab.common.dto.consumer;
 
 import kernel.maidlab.common.entity.consumer.Consumer;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import kernel.maidlab.common.enums.SocialType;
+import lombok.*;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ConsumerMyPageDto {
     private String name;
     private int point;
     private String profileImage;
-
-    @Builder
-    public ConsumerMyPageDto(String name, int point, String profileImage) {
-        this.name = name;
-        this.point = point;
-        this.profileImage = profileImage;
-    }
+    private SocialType socialType;
 
     public static ConsumerMyPageDto getInstance(Consumer consumer){
 
         return new ConsumerMyPageDto(
                 consumer.getName(),
                 consumer.getPoint(),
-                consumer.getProfileImage()
+                consumer.getProfileImage(),
+                consumer.getSocialType()
         );
     }
 }
