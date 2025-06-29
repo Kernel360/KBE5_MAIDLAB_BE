3e93dd90bacd8b5f92339e7a3da700cdf41ad705
JINS-DE <rlawlstjd044@naver.com>
2025-06-30 00:59:52 +0900

    feat : 매니저, 수요자 리뷰 등록시 키워드 별 등록

--- common/src/main/java/kernel/maidlab/common/entity/reservation/ReviewKeyword.java
+++ common/src/main/java/kernel/maidlab/common/entity/reservation/ReviewKeyword.java
@@ -10,7 +10,11 @@ import lombok.NoArgsConstructor;
 @Table(name = "review_keyword")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ReviewKeyword extends TimeBase {
+public class ReviewKeyword {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
 	// 리뷰 연관관계
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "review_id", nullable = false)
