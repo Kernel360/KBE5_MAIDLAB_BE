837f3215d2411a11e9c1be31a88f5c8e203a4da8
NeedIDcard <jo3902@gmail.com>
2025-07-15 21:10:40 +0900

    refactor/be/#247 테스트용2

--- core/src/main/java/kernel/maidlab/core/security/config/SecurityConfig.java
+++ core/src/main/java/kernel/maidlab/core/security/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
 					"/favicon.ico",
 					"/error",
 					"/admin/logs/stream",
-					"/nodeexport/prometheus"
+					"/nodeexport/prometheus",
+					"/actuator/**"
 					// WebSocket 엔드포인트
 				).permitAll()
 
