097821ca849fb189a866978aa93e10bab5695c82
JINS-DE <rlawlstjd044@naver.com>
2025-07-17 15:01:08 +0900

    fix : 추가 옵션 가격 수정

--- domain/src/main/java/kernel/maidlab/domain/manager/enums/ServiceOptionType.java
+++ domain/src/main/java/kernel/maidlab/domain/manager/enums/ServiceOptionType.java
@@ -3,9 +3,9 @@ package kernel.maidlab.domain.manager.enums;
 import java.math.BigDecimal;
 
 public enum ServiceOptionType {
-    WINDOW_CLEANING("창문 유리/커튼 및 블라인드 청소", 90, BigDecimal.valueOf(27900), false), FAN_CLEANING("선풍기 청소", 20,
-            BigDecimal.valueOf(7800), true), SHOES_CLEANING("운동화 세탁", 30, BigDecimal.valueOf(8600), true), IRONING("다림질",
-            60, BigDecimal.valueOf(15600), false);
+    WINDOW_CLEANING("창문 유리/커튼 및 블라인드 청소", 60, BigDecimal.valueOf(21900), false), FAN_CLEANING("선풍기 청소", 10,
+            BigDecimal.valueOf(4600), true), SHOES_CLEANING("운동화 세탁", 20, BigDecimal.valueOf(5900), true), IRONING("다림질",
+            50, BigDecimal.valueOf(12600), false);
 
     private final String label;
     private final int timeAddMinutes;
